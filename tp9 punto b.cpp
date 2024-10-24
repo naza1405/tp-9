6a882abff2ae1a3b197e0843b0fbc593decc76d6
naza1405 <garridonazareno14@gmail.com>
2024-10-24 10:23:17 -0300

    Add files via upload

---
+++ tp9 punto b.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+//funcion de datos//
+int datos(int &nota1, int &nota2, int &nota3, string &nombre, string &apellido, string &curso, string &materia);
+int datos(int &nota1, int &nota2, int &nota3, string &nombre, string &apellido, string &curso, string &materia){
+
+
+	
+cout<<"ingrese tu nombre: ";
+cin>>nombre;
+cout<<"ingrese tu apellido: ";
+cin>>apellido;
+cout<<"ingrese su curso: ";
+cin>>curso;
+cout<<"Ingrese su materia: ";
+cin>>materia;
+cout<<"ingrese sus 3 notas correspondientes a su trimestre"<<endl;
+cin>>nota1>>nota2>>nota3;	
+
+}
+
+int contarPromedio(bool &aprobado, int nota1, int nota2, int nota3, double &promedio);
+int contarPromedio(bool &aprobado, int nota1, int nota2, int nota3, double &promedio)
+{
+	
+	promedio=(nota1+nota2+nota3) / 3;
+	
+	if(promedio >= 7){
+		aprobado = true;
+	}
+	else{
+		aprobado = false;
+	}
+}
+
+//funcion de ramon condicion//
+int condicion(bool aprobado, string nombre, string apellido, string curso, string materia, double promedio);
+int condicion(bool aprobado, string nombre, string apellido, string curso, string materia, double promedio){
+
+string desaprobado;
+
+if(aprobado == true){
+	
+	desaprobado = "aprobado";
+}
+
+else{
+	
+	desaprobado = "desaprobado";
+}
+	
+cout<<nombre<<" "<<apellido<<" - "<<materia<<" - "<<curso<<endl;
+cout<<"nota final: "<<promedio<<" ("<<desaprobado<<")"<<endl;
+
+
+
+}
+int main(){
+
+string nombre, apellido, curso, materia;
+int nota1, nota2, nota3;
+double promedio;
+bool aprobado;
+
+datos(nota1, nota2, nota3, nombre, apellido, curso, materia);
+cout<<endl;
+contarPromedio(aprobado, nota1, nota2, nota3, promedio);
+condicion(aprobado, nombre, apellido, curso, materia, promedio);
+
+}
+
+
+
+
+
